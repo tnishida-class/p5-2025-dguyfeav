ff05df6e95339444aa70f9ff0c7097d18497d823
dguyfeav <2491007h@gsuite.kobe-u.ac.jp>
2025-11-18 17:16:54 +0900

    Finalwork

--- Q2-3/sketch.js
+++ Q2-3/sketch.js
@@ -1,23 +1,38 @@
 // ギリシャ国旗（ほかの国旗に挑戦してもOK）
 function setup() {
-  createCanvas(270, 180);
-  noStroke();
-  background(255);
+createCanvas(270, 180);
+noStroke();
+background(255);
 
-  const d = height / 9; // 縞1本の太さ
-  const blue = color(0, 51, 160);
+const d = height / 9; // 縞1本の太さ
+const blue = color(0, 51, 160);//青色の表示方式（RBG）
 
-  // 縞を描く
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] ヒント：縞の色を交互に変えるには2で割った余りを使おう
-    rect(0, i * d, width, (i + 1) * d);
-  }
-
-  // 十字を描く
-  const size = d * 5;
-  fill(blue);
-  rect(0, 0, size, size);
-  fill(255);
-  rect(d * 2, 0, d, size);
-  // BLANK[2] 十字を完成させよう
+// 縞を描く
+for(let i = 0; i < 9; i++)//i を 0 から 9 まで繰り返し１ずつ足す
+// BLANK[1] ヒント：縞の色を交互に変えるには2で割った余りを使おう
+    {
+if(i % 2 == 0){//i が偶数かどうかを判定する条件式,% は割り算の余りを求める演算子だから、、i ÷ 2 の余りが 0ならば
+fill(blue)//線は青で 
+rect(0, i * d, width, (i + 1) * d);//rectは四角形。rect(xのスタート、ｙのスタート、ｘの終わり、ｙの終わり)
 }
+else{//仮定が成り立たないならば
+fill(255)
+rect(0, i * d, width, (i + 1) * d);
+}
+}
+// 十字を描く
+const size = d * 5;
+fill(blue);
+rect(0, 0, size, size);
+fill(255);
+rect(d * 2, 0, d, size);//rectは四角形。rect(xのスタート、ｙのスタート、ｘの終わり、ｙの終わり)
+fill(255);
+rect(0, d*2, size, d)
+}
+
+
+
+
+
+
+
--- Q5-2/sketch.js
+++ Q5-2/sketch.js
@@ -1,30 +1,43 @@
 // 吹き出し
 function setup(){
-  createCanvas(400, 400);
-  textSize(16);
+createCanvas(400, 400);
+textSize(16);
 }
 
 function draw(){
-  background(220);
-  balloon("関数は難しい？", 100, 100);
-  balloon("関数は便利？", mouseX, mouseY);
+background(220);
+balloon("関数は難しい？", 100, 100);
+balloon("関数は便利？", mouseX, mouseY);
 }
 
 function balloon(t, x, y){
-  const w = textWidth(t); // テキストの幅
-  const h = textAscent() + textDescent(); // テキストの高さ
-  const p = 4; // 余白の大きさ (padding)
+const w = textWidth(t); // テキストの幅
+const h = textAscent() + textDescent(); // テキストの高さ
+const p = 4; // 余白の大きさ (padding)
 
-  push();
+const rectW = w + p * 2; // 矩形の幅
+const rectH = h + p * 2; // 矩形の高さ
 
-  // BLANK[1] 吹き出しの背景を先に描く
+push();
 
-  // BLANK[2] 吹き出しの三角形を描く
+// BLANK[1] 吹き出しの背景を先に描く
+fill(20,70,70);
+noStroke();
+rect(x, y, rectW, rectH, 8); //xy横幅高さ丸み度
 
-  // 吹き出しのテキストを次に描く
-  textAlign(LEFT, CENTER);
-  fill(255);
-  text(t, x + p, y + h / 2 + p);
-  
-  pop();
+
+// BLANK[2] 吹き出しの三角形を描く
+fill(20,70,70);
+triangle(
+x + rectW - p*2, y + rectH, // 左上
+x + rectW - p/2, y + rectH - 30, // 右上
+x + rectW, y + rectH + 15 // 下
+);
+
+// 吹き出しのテキストを次に描く
+textAlign(LEFT, CENTER);
+fill(255);
+text(t, x + p, y + h / 2 + p);
+
+pop();
 }
